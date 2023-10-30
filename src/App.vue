b6ff3259d63f4a6ac7810d9ce088f1f6cf74e59c
root <root@ip-172-31-46-174.ap-northeast-1.compute.internal>
2023-10-30 09:52:05 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,16 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
 }
+:root {
+  --first-color: #010409;
+  --second-color: #0d1118;
+  --navbar-color: #161b22;
+  --button-color: #21262d;
+  --grey-color: #c9d1d9;
+  --grey-two-color: #8b949e;
+  --line-color: #30363d;
+  --input-color: #0d1117;
+  --border-color: #30363d;
+  --blue-text--color: #58a6ff;
+}
 </style>
