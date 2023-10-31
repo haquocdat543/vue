704418d2a907d2275a076e9fe200abbcf2068a67
haquocdat543 <wwwdatha543@gmail.com>
2023-10-31 14:02:21 +0000

    add NavCenter

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,18 @@
 <template>
 /* Left */
-<div>
+<div class="navLeft">
 	<img src="../images/img1.png" alt="">
 	<div>
 		<input type="text" placeholder="Search or jump to">
 	</div>
 </div>
 /* Center */
+<div class="navCenter">
+	<h2>Pulls</h2>
+	<h2>Issues</h2>
+	<h2>Marketplace</h2>
+	<h2>Explore</h2>
+</div>
 /* Right */
 </template>
 
