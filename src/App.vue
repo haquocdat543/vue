fe5fab35e14c17c4f48df1d31fca61c6fd6d62bc
haquocdat543 <wwwdatha543@gmail.com>
2023-10-31 13:17:02 +0000

    Add color

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <template>
-  <div id="app">
-    <TheHeader />
-    <TodoList />
-  </div>
+  
 </template>
 
 <script>
-import TheHeader from './components/TheHeader'
-import TodoList from "./components/TodoList"
 
 export default {
   name: 'App',
-  components: { TheHeader, TodoList }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
 :root {
   --first-color: #010409;
   --second-color: #0d1118;
@@ -29,7 +20,8 @@ export default {
   --line-color: #30363d;
   --input-color: #0d1117;
   --border-color: #30363d;
-  --blue-text--color: #58a6ff;
-  background: var(--first-color);
+  --blue-text-color: #58a6ff;
+  background-color: var(--first-color);
 }
 </style>
+
